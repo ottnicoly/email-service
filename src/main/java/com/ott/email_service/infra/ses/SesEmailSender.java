52230278781f07d3d8e7aaa8520ab10bb29a74ce
Nicoly Ott <nicolyott23@gmail.com>
2024-11-08 14:17:22 -0300

    camada de infraestrutura

---
+++ src/main/java/com/ott/email_service/infra/ses/SesEmailSender.java
@@ -0,0 +1,35 @@
+package com.ott.email_service.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.ott.email_service.adapters.EmailSenderGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SesEmailSender implements EmailSenderGateway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService){
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("nicolyott23@gmail.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body)))
+                );
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        }catch (AmazonServiceException e) {
+            throw new EmailServiceException("Failure while sendibng email");
+        }
+    }
+}
