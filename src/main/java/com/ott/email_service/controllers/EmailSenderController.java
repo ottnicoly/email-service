6057259522a6767d2411a6d6a4c252fb18426fea
Nicoly Ott <nicolyott23@gmail.com>
2024-11-08 14:48:47 -0300

    criando o controller

---
+++ src/main/java/com/ott/email_service/controllers/EmailSenderController.java
@@ -0,0 +1,34 @@
+package com.ott.email_service.controllers;
+
+import com.ott.email_service.application.EmailSenderService;
+import com.ott.email_service.core.exceptions.EmailServiceException;
+import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderService emailSenderService) {
+        this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request){
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("email send successfully");
+        } catch (EmailServiceException e){
+            return ResponseEntity.status(HttpStatus.SC_INTERNAL_SERVER_ERROR).body("Error while send email");
+        }
+    }
+
+}
