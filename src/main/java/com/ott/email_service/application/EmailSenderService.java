1f577b6fea3e93c5bd9f994617750906840ac2f2
Nicoly Ott <nicolyott23@gmail.com>
2024-11-08 13:42:46 -0300

    camada application

---
+++ src/main/java/com/ott/email_service/application/EmailSenderService.java
@@ -0,0 +1,23 @@
+package com.ott.email_service.application;
+
+import com.ott.email_service.adapters.EmailSenderGateway;
+import com.ott.email_service.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+
+}
